e2bcbe7d8394b90aa9a6cad5ce6a52355a600f85
Ahmet Buğra Aydın <bugraaydin1@hotmail.com>
2024-03-30 18:52:01 +0300

    add pulicPath for S3 bucket path

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,6 +9,7 @@ module.exports = merge(webpackCommon, {
 	mode: "production",
 	output: {
 		filename: "[name].[contenthash].js",
+		publicPath: "/container/latest/",
 	},
 	plugins: [
 		new ModuleFederationPlugin({
